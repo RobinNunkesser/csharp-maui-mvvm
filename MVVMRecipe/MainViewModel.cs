7af36e4dd2bc457a2cb3fd27fa6f4771bdff0471
nunkesser <robin.nunkesser@hshl.de>
2024-05-24 12:11:55 +0200

    Better INotifyPropertyChanged implementation

--- MVVMRecipe/MainViewModel.cs
+++ MVVMRecipe/MainViewModel.cs
@@ -2,60 +2,55 @@
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
-namespace MVVMRecipe
+namespace MVVMRecipe;
+
+public sealed class MainViewModel : INotifyPropertyChanged
 {
-    public class MainViewModel : INotifyPropertyChanged
+    private string _forename = string.Empty;
+
+    private Command? _resetCommand;
+
+    private string _surname = string.Empty;
+
+    public string Forename
     {
-        private string forename = string.Empty;
-        public string Forename
-        {
-            get => forename;
-            set
-            {
-                if (forename != value)
-                {
-                    forename = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-        private string surname = string.Empty;
-        public string Surname
-        {
-            get => surname;
-            set
-            {
-                if (surname != value)
-                {
-                    surname = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public ICommand ResetCommand { get; private set; }
-
-        public MainViewModel()
-        {
-            ResetCommand = new Command(Reset);
-        }
-
-        public void Reset()
-        {
-            Forename = string.Empty;
-            Surname = string.Empty;
-        }
-
-        #region INotifyPropertyChanged implementation
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public void OnPropertyChanged(
-            [CallerMemberName] string name = ""
-        ) =>
-            PropertyChanged?.Invoke(
-                this,
-                new PropertyChangedEventArgs(name)
-            );
-        #endregion
+        get => _forename;
+        set => SetField(ref _forename, value);
     }
+
+    public string Surname
+    {
+        get => _surname;
+        set => SetField(ref _surname, value);
+    }
+
+    public ICommand ResetCommand => _resetCommand ??= new Command(Reset);
+
+    private void Reset()
+    {
+        Forename = string.Empty;
+        Surname = string.Empty;
+    }
+
+    #region INotifyPropertyChanged implementation
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged(
+        [CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this,
+            new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool SetField<T>(ref T field, T value,
+        [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    #endregion
 }
